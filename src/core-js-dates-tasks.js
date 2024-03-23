1c9e6ab08e44eb0b19f021027531e0f284fcc9f3
alekseng <alexvdche@mail.ru>
2024-03-23 23:44:49 +0300

    feat: complete 5/14 assignments

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -100,8 +100,10 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const date1 = new Date(dateStart).getTime();
+  const date2 = new Date(dateEnd).getTime();
+  return (date2 - date1) / 86400 / 1000 + 1;
 }
 
 /**
@@ -121,8 +123,14 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const targetDate = new Date(date).getTime();
+  const start = new Date(period.start).getTime();
+  const end = new Date(period.end).getTime();
+  if (targetDate >= start && targetDate <= end) {
+    return true;
+  }
+  return false;
 }
 
 /**
